129f4d3be282ca0571007025e8a1300a9a339d90
Mason Wang <ycda@huang-l40s-1.csail.mit.edu>
2025-05-30 16:38:10 -0400

    updated training

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import hydra
 import wandb
@@ -16,6 +17,17 @@ def main(config):
 
     torch.set_float32_matmul_precision(config.float32_matmul_precision)
 
+    # Finding where we should load the checkpoint
+    if config.ckpt_path is not None:
+        ckpt_path = config.ckpt_path
+        print(f"Resuming from provided checkpoint: {ckpt_path}")
+    elif config.load_last_if_avail and os.path.exists("checkpoints/last.ckpt"):
+        ckpt_path = "checkpoints/last.ckpt"
+        print(f"Resuming from last checkpoint: {ckpt_path}")
+    else:
+        ckpt_path = None
+        print("No checkpoint provided or found. Training from scratch.")
+
     data_module = BaseDataModule(config.data)
     lit_module = FMDiffAEModule(config)
 
@@ -23,11 +35,9 @@ def main(config):
         lit_module = torch.compile(lit_module)
 
     callbacks = [instantiate(c) for c in config.callbacks.values()]
-
-    logger = instantiate(config.logger)
+    logger = instantiate(config.logger, resume=("auto" if ckpt_path else "never"))
     trainer = instantiate(config.trainer, callbacks=callbacks, logger=logger)
-
-    trainer.fit(lit_module, data_module, ckpt_path=config.ckpt_path)
+    trainer.fit(lit_module, data_module, ckpt_path=ckpt_path)
 
 
 if __name__ == "__main__":

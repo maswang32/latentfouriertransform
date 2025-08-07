4660401429c55a1589528d1aa828b32368eb3823
Mason Wang <ycda@huang-l40s-1.csail.mit.edu>
2025-08-07 17:55:16 -0400

    updated spectral sweep code

--- reproduce_results/classification/generate_with_spectral_sweep.py
+++ reproduce_results/classification/generate_with_spectral_sweep.py
@@ -1,8 +1,15 @@
 import argparse
+import os
 import math
 import numpy as np
 import torch
+import torch.nn.functional as F
+import librosa
 from fmdiffae.lightning.lit_fmdiffae import FMDiffAEModule
+
+
+def get_sliding_window_mask(length, window_size, step_size):
+    return torch.eye(length).unfold(0, window_size, step_size).sum(dim=-1)
 
 
 def generate_with_spectral_sweep(
@@ -11,7 +18,9 @@ def generate_with_spectral_sweep(
     step_size,
     batch_size,
     device,
-    inputs,
+    save_path=None,
+    save_interval=None,
+    inputs=None,
     cfg_scale=3.0,
     init_noise=None,
     num_steps=35,
@@ -24,10 +33,10 @@ def generate_with_spectral_sweep(
     num_bands = math.ceil((rfft_size - window_size + 1) / step_size)
 
     # Create sliding window mask
-    toeplitz = torch.eye(rfft_size).unfold(0, window_size, step_size).sum(dim=-1)
+    sliding_window_mask = get_sliding_window_mask(rfft_size, window_size, step_size)
 
     # Expand to be compatible
-    toeplitz = toeplitz.repeat(num_inputs, 1)
+    sliding_window_mask = sliding_window_mask.repeat(num_inputs, 1)
     inputs = inputs.repeat_interleave(num_bands, dim=0)
 
     if init_noise is not None:
@@ -38,8 +47,10 @@ def generate_with_spectral_sweep(
     out = model.batch_generate(
         batch_size=batch_size,
         device=device,
+        save_path=save_path,
+        save_interval=save_interval,
         inputs=inputs,
-        fft_mask=toeplitz,
+        fft_mask=sliding_window_mask,
         cfg_scale=cfg_scale,
         init_noise=init_noise,
         num_steps=num_steps,
@@ -49,18 +60,44 @@ def generate_with_spectral_sweep(
     return out.unflatten(0, (num_inputs, num_bands))
 
 
+def jensen_shannon_distance(p_logits, q_logits, dim=-1):
+    p_log = F.log_softmax(p_logits, dim=dim)
+    q_log = F.log_softmax(q_logits, dim=dim)
+
+    m_log = torch.logaddexp(p_log, q_log) - math.log(2.0)
+
+    # Note - F.kl_div's arguments are (input, target)
+    # This is the reverse of the math notation, in which the "true" distribution
+    # Is the first argument
+    kl1 = F.kl_div(m_log, p_log, log_target=True, reduction="none").sum(dim)
+    kl2 = F.kl_div(m_log, q_log, log_target=True, reduction="none").sum(dim)
+    return torch.sqrt(0.5 * (kl1 + kl2))
+
+
+def compute_beat_spectrum(x, fs=22050, hop_length=256, max_size=None):
+    oenv = librosa.onset.onset_strength(y=x, sr=fs, hop_length=hop_length)
+    autocorr = librosa.autocorrelate(oenv, max_size=max_size)
+    return autocorr / np.max(autocorr)
+
+
+def estimate_tempo(x, fs=22050, hop_length=256):
+    oenv = librosa.onset.onset_strength(y=x, sr=fs, hop_length=hop_length)
+    tempo = librosa.feature.tempo(onset_envelope=oenv, sr=fs, hop_length=hop_length)
+    return tempo
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate Audio Using Sliding Windows")
     parser.add_argument(
         "ckpt_path",
     )
     parser.add_argument(
-        "save_path",
+        "save_dir",
     )
     parser.add_argument(
-        "--batch_size",
-        type=int,
-        default=256,
+        "--same_init_noise",
+        action="store_true",
+        default=False
     )
     parser.add_argument(
         "--window_size",
@@ -71,6 +108,11 @@ if __name__ == "__main__":
         "--step_size",
         type=int,
         default=1,
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=256,
     )
     parser.add_argument(
         "--cfg_scale",
@@ -84,11 +126,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    # Load Data
+    # Load Data/Model
     valid_gtzan_spec = torch.from_numpy(
         np.load("/data/hai-res/ycda/processed-datasets/gtzan/valid_spec.npy")
     )
-
     model = FMDiffAEModule.load_torch_model(
         ckpt_path=args.ckpt_path,
         strict=True,
@@ -96,21 +137,27 @@ if __name__ == "__main__":
 
     model = torch.compile(model)
 
-    gen = torch.Generator()
-    gen.manual_seed(3)
-    init_noise = torch.randn(*model.datashape, generator=gen) * 80
+    if args.same_init_noise:
+        gen = torch.Generator()
+        gen.manual_seed(3)
+        init_noise = torch.randn(*model.datashape, generator=gen) * 80
+    else:
+        init_noise = None
 
-    out = generate_with_spectral_sweep(
-        model,
-        window_size=args.window_size,
-        step_size=args.step_size,
-        batch_size=args.batch_size,
-        device=next(model.parameters()).device,
-        inputs=valid_gtzan_spec,
-        cfg_scale=args.cfg_scale,
-        init_noise=init_noise,
-        num_steps=args.num_steps,
-        outer_pbar=True,
-        inner_pbar=False,
-    )
-    torch.save(out, args.save_path)
+    for i in range(valid_gtzan_spec[0]):
+        out = generate_with_spectral_sweep(
+            model,
+            window_size=args.window_size,
+            step_size=args.step_size,
+            batch_size=args.batch_size,
+            device=next(model.parameters()).device,
+            save_path=None,
+            save_interval=None,
+            inputs=valid_gtzan_spec[i].unsqueeze(0),
+            cfg_scale=args.cfg_scale,
+            init_noise=init_noise,
+            num_steps=args.num_steps,
+            outer_pbar=True,
+            inner_pbar=False,
+        )
+        torch.save(out, os.path.join(args.save_dir, "{i}.pt"))

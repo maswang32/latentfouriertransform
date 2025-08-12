8620e570dcd570f3598eaf06bc27134b2d60d926
Mason Wang <ycda@huang-l40s-1.csail.mit.edu>
2025-08-12 04:18:04 -0400

    updated lightning module to accept new learning rates for old checkpoints

--- fmdiffae/lightning/lit_fmdiffae.py
+++ fmdiffae/lightning/lit_fmdiffae.py
@@ -36,6 +36,14 @@ class FMDiffAEModule(L.LightningModule):
             "lr_scheduler": {"scheduler": scheduler, "interval": "step"},
         }
 
+    def on_train_start(self):
+        target_lr = self.hparams["optimizer"]["lr"]
+        for optimizer in self.trainer.optimizers:
+            for param_group in optimizer.param_groups:
+                old_lr = param_group["lr"]
+                param_group["lr"] = target_lr
+                print(f"Changed LR from {old_lr} to {param_group['lr']}")
+
     def training_step(self, batch):
         loss = self(batch)
         self.log("loss/train", loss, prog_bar=True, on_step=True, on_epoch=False)

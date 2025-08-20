925fbe22335c9321c81dac347c8b9d64fde13e13
Mason Wang <ycda@huang-l40s-1.csail.mit.edu>
2025-08-20 16:35:51 -0400

    added ablations to fft mask - ablating logscale and correlations

--- fmdiffae/arc/correlated_fft_mask.py
+++ fmdiffae/arc/correlated_fft_mask.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 
 class CorrelatedFFTMask(nn.Module):
-    def __init__(self, n_fft=1024, sigma=0.5, p=2, eps=1e-6):
+    def __init__(self, n_fft=1024, sigma=0.5, p=2, logscale=True, eps=1e-6):
         super().__init__()
         assert math.log2(n_fft).is_integer(), "n_fft must be a power of 2"
 
@@ -15,13 +15,21 @@ class CorrelatedFFTMask(nn.Module):
             "v", torch.linspace(0, 1, self.F), persistent=False
         )  # Normalized Frequencies
 
-        self.log_v = torch.log(self.v + eps)  # Normalized freqs in log space
-
         # Transformation used correlate fft bin scores
-        k = torch.exp(
-            -0.5 * ((torch.abs(self.log_v[:, None] - self.log_v[None, :]) / sigma) ** p)
-        )
-        k = k / torch.sqrt(torch.sum(k**2, axis=0, keepdim=True))
+        if sigma > 0:
+            if logscale:
+                self.log_v = torch.log(self.v + eps)  # Normalized freqs in log space
+                axis = self.log_v
+            else:
+                axis = self.v
+
+            k = torch.exp(
+                -0.5 * ((torch.abs(axis[:, None] - axis[None, :]) / sigma) ** p)
+            )
+            k = k / torch.sqrt(torch.sum(k**2, axis=0, keepdim=True))
+        else:
+            k = torch.eye(n_fft)
+
         self.register_buffer("k", k, persistent=False)
 
     def forward(self, x, lows=None, highs=None, fft_mask=None):

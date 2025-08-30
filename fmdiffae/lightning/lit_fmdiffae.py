77a16a5f1c85ab3322bef4bb357046638fd281d8
Mason Wang <ycda@huang-l40s-1.csail.mit.edu>
2025-08-30 16:02:50 -0400

    added map location cpu deleted prints

--- fmdiffae/lightning/lit_fmdiffae.py
+++ fmdiffae/lightning/lit_fmdiffae.py
@@ -63,8 +63,6 @@ class FMDiffAEModule(L.LightningModule):
 
     @classmethod
     def load_torch_model(cls, ckpt_path, strict=True):
-        print("HYO")
         lit = cls.load_from_checkpoint(ckpt_path, strict=strict, map_location="cpu")
-        print(lit.device)
         print("Loading EMA Model?", hasattr(lit, "ema_model"))
         return lit.ema_model.module if hasattr(lit, "ema_model") else lit.model
